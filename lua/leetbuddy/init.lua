34e58d4e33d7d15492ebb75fabd5620ae5ce0cd1
Dhanus <dhanus3133@gmail.com>
2023-05-30 00:54:03 +0530

    Fix bug for custom user_config

--- lua/leetbuddy/init.lua
+++ lua/leetbuddy/init.lua
@@ -2,8 +2,30 @@ local M = {}
 
 M.user_config = require("leetbuddy.default_config")
 
+local function create_cmds()
+  local questions = require("leetbuddy.questions").questions
+  local question = require("leetbuddy.questionfromfile").question
+  local reset = require("leetbuddy.reset").reset_question
+  local split = require("leetbuddy.split").split
+  local test = require("leetbuddy.runner").test
+  local submit = require("leetbuddy.runner").submit
+  local checkcookies = require("leetbuddy.cookies").check_auth
+
+  local opts = {}
+
+  vim.api.nvim_create_user_command("LBQuestions", questions, opts)
+  vim.api.nvim_create_user_command("LBQuestion", question, opts)
+  vim.api.nvim_create_user_command("LBReset", reset, opts)
+  vim.api.nvim_create_user_command("LBSplit", split, opts)
+  vim.api.nvim_create_user_command("LBTest", test, opts)
+  vim.api.nvim_create_user_command("LBSubmit", submit, opts)
+  vim.api.nvim_create_user_command("LBClose", "qall", opts)
+  vim.api.nvim_create_user_command("LBCheckCookies", checkcookies, opts)
+end
+
 M.setup = function(opts)
   M.user_config = vim.tbl_deep_extend("force", M.user_config, opts)
+  create_cmds()
 end
 
 return M
